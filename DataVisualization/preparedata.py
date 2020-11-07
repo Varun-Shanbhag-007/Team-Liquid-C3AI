0fd4ac1317a2859b3b3b4aee7b6f4c0bfd2abd57
Shivam Kulkarni <shivamkulkarnimu10@gmail.com>
2020-11-07 13:00:08 -0600

    Update preparedata.py

--- DataVisualization/preparedata.py
+++ DataVisualization/preparedata.py
@@ -1,5 +1,7 @@
+import requests
 import pandas as pd
 import numpy as np
+from matplotlib import pyplot as plt
 import c3aidatalake
 
 def get_simulation_data(county, start_date, end_date):
@@ -72,3 +74,55 @@ def get_simulation_data(county, start_date, end_date):
     prob_visiting_park = (mobility_trends[county+".Google_ParksMobility.data"].values * (us_percent_people_going_to_parks/100))/100
 
     return confirmed_cases, confirmed_deaths, confirmed_recoveries, infection_rate, mortality_rate, recovery_rate, county_total_pop, county_pop_density, prob_visiting_grocery_store, prob_visiting_restaurant, prob_visiting_park
+
+def plot_SIR_graph(county, start_date, end_date, plot_susceptible):
+    locations = c3aidatalake.fetch(
+        "outbreaklocation",
+        {
+            "spec" : {
+                "filter": "contains(id,'"+county+"')"
+            }
+        }
+        , get_all = True
+    )
+
+    population = int(locations['populationCDS'].values[0])
+    
+    casecounts = c3aidatalake.evalmetrics(
+        "outbreaklocation",
+        {
+            "spec" : {
+                "ids" : [county],
+                "expressions" : [ "CDS_Cases", "CDS_Deaths","CDS_Recovered"],
+                "start" : start_date,
+                "end" : end_date,
+                "interval" : "DAY",
+            }
+        }
+    )
+
+    casecounts = casecounts[['dates', county+'.CDS_Cases.data', county+'.CDS_Recovered.data', county+'.CDS_Deaths.data']]
+    casecounts[county+'.Active_Cases.data'] = casecounts[county+'.CDS_Cases.data'] - casecounts[county+'.CDS_Recovered.data'] - casecounts[county+'.CDS_Deaths.data']
+    casecounts[county+'.Susceptible.data'] = population - casecounts[county+'.CDS_Cases.data'] - casecounts[county+'.CDS_Recovered.data'] - casecounts[county+'.CDS_Deaths.data']
+    
+    plt.figure(figsize = (8, 6))
+    plt.plot(
+        casecounts["dates"],
+        casecounts[county+".Active_Cases.data"],
+        label = "I"
+    )
+    plt.plot(
+        casecounts["dates"],
+        casecounts[county+".CDS_Recovered.data"],
+        label = "R"
+    )
+    if plot_susceptible:
+        plt.plot(
+            casecounts["dates"], 
+            casecounts[county+".Susceptible.data"],
+            label = "S"
+        )
+    plt.legend()
+    plt.xticks(rotation = 45, ha = "right")
+    plt.ylabel("Count")
+    plt.show()
